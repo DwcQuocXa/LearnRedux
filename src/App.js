631bde3a94c3fb40f595f4034e588037e063b16d
DucQuocXa <duc.ngogia310302@gmail.com>
2021-09-28 11:15:00 +0300

    basic of Redux done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement, signIn } from "./actions";
 
 function App() {
   const counter = useSelector((state) => state.counter);
+  const isLogged = useSelector((state) => state.isLogged);
+  const dispatch = useDispatch();
   return (
     <div className="App">
       <h1>Counter {counter}</h1>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+      <button onClick={() => dispatch(signIn())}>
+        {isLogged ? "Log out" : "Log in"}
+      </button>
+      {isLogged ? <h3>Logged in</h3> : ""}
     </div>
   );
 }
